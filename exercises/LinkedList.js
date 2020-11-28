bc49c6c752f62d0f74b6c29170a69ad3d493d1fd
madewithabandon <marissabriesmith@gmail.com>
2020-11-28 14:41:47 -0800

    added remove function

--- exercises/LinkedList.js
+++ exercises/LinkedList.js
@@ -29,16 +29,42 @@ class LinkedList {
             current.next = new_node;
         }
         this.size++;
+        return this;
     }
 
     // If the value is in the list, remove the node. If the value is not in the list, throw an error.
-    remove() {
+    remove(value) {
+        var current = this.head;
+        if (current.value == value){
+            this.head = current.next;
+            this.size--;
+            return this;
+        }
 
+        while(current.next != null){
+            if (current.next.value == value){
+                current.next =current.next.next;
+                this.size--;
+                return this;
+            }
+            current = current.next;
+        }
+        throw new Error("value not found")
     }
 
     // Print the linked list.
     print() {
-
+        if (!this.head){
+            console.log("the list is empty");
+            return this;
+        }
+        var current = this.head;
+        while(current.next != null){
+            console.log(current.value);
+            current = current.next;
+        }
+        console.log(current.value);
+        return this;
     }
 
     // 
@@ -52,8 +78,8 @@ module.exports = LinkedList;
 
 /* EXTRA CREDIT 
 
-  - What is the O(n) of the `add` function?
-  - What is the O(n) of the `remove` function?
+  - What is the O of the `add` function?
+  - What is the O of the `remove` function?
   - Make `add` and `remove` "chainable"
   - Make it a "Doubly-Linked List" by adding a `previous` to nodes
   - How could you change the data structure to make adding an element O(1)
