a49b9a528bb44053b8c57e609477fd6da999ed5d
Michael Smith <michaelsmith.a@gmail.com>
2020-11-28 08:53:33 -0800

    Adjust stack to work

--- exercises/Stack.js
+++ exercises/Stack.js
@@ -4,16 +4,21 @@ class Stack {
         this.size = 0;
     }
 
+    // Add an item to the top of the stack
     push() {
 
     }
 
+    // Remove an item from the top of the stack and return it. Return `null` if the stack is empty.
     pop() {
 
     }
 
+    // Return the item on top of the stack
     peek() {
 
     }
 
 }
+
+module.exports = Stack;
